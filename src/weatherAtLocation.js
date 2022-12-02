200087ec42d653c8c4ee8f43e5b89815756783c2
Boris-Boef <frikandelbroodje18@gmail.com>
2022-12-02 11:45:42 +0100

    changed the code to show the weather at different places in different graphs

--- src/weatherAtLocation.js
+++ src/weatherAtLocation.js
@@ -1,9 +1,10 @@
-var url = "https://api.open-meteo.com/v1/forecast?latitude=52.2&longitude=8.9&hourly=temperature_2m"
+var baseUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.2&longitude=8.9&hourly=temperature_2m"
 var url2 = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population'
 
-getData();
+async function getData(latitude, longitude, chartName){
+    
+    var url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
 
-async function getData(){
     const response = await fetch(url);
     //console.log(response);
     const data = await response.json();
@@ -19,7 +20,7 @@ async function getData(){
         temperature_2m.push(data.hourly.temperature_2m[i]);
     }
 
-    new Chart(document.getElementById("myChart"),{
+    new Chart(document.getElementById(chartName),{
         type: 'line',
         data:{
             labels: time,
