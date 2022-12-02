0a6569393682bf5116e3f1727e266bc2be8cc72c
Esther Belinfante <estherbelinfante@gmail.com>
2022-12-02 11:30:49 +0100

    cleaned up unnecessary code

--- src/test_weather_api.js
+++ src/test_weather_api.js
@@ -1,11 +1,9 @@
-// var url = 'https://api.open-meteo.com/v1/forecast?current_weather=true&timezone=Europe%2FBerlin&latitude=52.2&longitude=8.9';
-// var url2 = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
-var url3 = 'https://api.open-meteo.com/v1/forecast?latitude=52.2&longitude=8.9&hourly=temperature_2m';
+var url = 'https://api.open-meteo.com/v1/forecast?latitude=52.2&longitude=8.9&hourly=temperature_2m';
 
 getData();
 
 async function getData(){
-    const response = await fetch(url3);
+    const response = await fetch(url);
     console.log(response);
     const data = await response.json();
     console.log(data);
@@ -40,84 +38,3 @@ async function getData(){
         }
     });
 }
-
-
-
-
-
-
-// getData();
-
-// async function getData() {
-//     const response = await fetch(url2);
-//     console.log(response);
-//     const data = await response.json();
-//     console.log(data);
-//     length = data.data.length;
-//     console.log(length);
-
-//     labels = [];
-//     values = [];
-//     for (i = 0; i < length; i++) {
-//         labels.push(data.data[i].Year);
-//         values.push(data.data[i].Population);
-//     }
-
-//     new Chart(document.getElementById("bar-chart"), {
-//         type: 'bar',
-//         data: {
-//             labels: labels,
-//             datasets: [
-//                 {
-//                     label: "Population (millions)",
-//                     backgroundColor: ["#3e95cd",
-//                                       "#8e5ea2", 
-//                                       "#3cba9f", 
-//                                       "#e8c3b9", 
-//                                       "#c45850",
-//                                       "#CD5C5C", 
-//                                       "#40E0D0"],
-//                     data: values
-//                 }
-//             ]
-//         },
-//         options: {
-//             legend: { display: false },
-//             title: {
-//                 display: true,
-//                 text: 'U.S population'
-//             }
-//         }
-//     });
-// }
-
-// fetch(url)
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-
-
-// fetch(url)
-//   .then(response => {
-//     return response.json();
-//     })
-//   .then(data => {
-//     test = data;
-//     return console.log(data); 
-//   })
-
-//   console.log(test);
-
-
-// async function fetchWeatherJSON() {
-//     const response = await fetch(url);
-
-//     if (!response.ok) {
-//         throw new Error('HTTP error! status: ${response.status}');
-//     }
-//     const data = await response.json(); // Extracting data as a JSON Object from the response
-//     return data;
-// }
-
-// fetchWeatherJSON().then(data => { console.log(data); })
-
-
