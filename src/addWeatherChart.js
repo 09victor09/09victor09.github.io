ce1ecbad2c5a2409c7c3535083cb7f65c6e3dc3a
Boris-Boef <frikandelbroodje18@gmail.com>
2022-12-21 13:20:27 +0100

    fetching data from api showing charts from selection

--- src/addWeatherChart.js
+++ src/addWeatherChart.js
@@ -1,9 +1,9 @@
 async function getDataFromUrl(url){
     const response = await fetch(url,{
         method: "get",
-        mode: "no-cors",
         headers: new Headers({
           "ngrok-skip-browser-warning": "69420",
+          "Content-Type": "application/json"
         })
     }).catch((error) =>{
         alert(error);
@@ -11,74 +11,46 @@ async function getDataFromUrl(url){
     return response.json();
 }
 
-async function addWeatherChart(location){
+async function addWeatherChart(output_type, devices){
 
-    var url = "https://6f63-2001-1c06-180b-b600-c6dd-83ad-209c-52e5.eu.ngrok.io/webserver/api/";
+    var url = "https://6bd4-2001-1c06-180b-b600-c6dd-83ad-209c-52e5.eu.ngrok.io/webserver/api/";
 
-    const data = await getDataFromUrl(url)
-    .catch((error) => {
-        alert(error)
+    var data = await getDataFromUrl(url);
+
+    console.log(data)
+
+    time = [];
+    temperature_2m = [];
+
+    for(i=0; i< data[devices[0]].entry_hour.length; i++){
+        time.push(data[devices[0]].entry_hour[i]);
+        temperature_2m.push(data[devices[0]][output_type][i]);
+    }
+
+    removeElement("div1");
+    
+    addElement("div1", "canvas", location);
+        
+    new Chart(document.getElementById(location),{
+        type: 'line',
+        data:{
+            labels: time,
+            datasets: [
+                {
+                    label: output_type,
+                    data: temperature_2m
+                }
+            ]
+        },
+        options: {
+            legend: { display: false },
+            plugins:{
+                title: {
+                    display: true,
+                    text: devices[0]
+                }
+            }
+        }
     });
-    alert(data);
-
-    // const response = await fetch(url, {
-    //     method: "get",
-    //     mode: "no-cors",
-    //     headers: new Headers({
-    //       "ngrok-skip-browser-warning": "69420",
-    //     })
-    // }).catch((error) =>{
-    //     alert(error);
-    // }).then((response) => response.json())
-    // .then((data) => alert(data));
-
-
-    // const data = await response.json();
-    // alert(data);
-        
-    // time = [];
-    // temperature_2m = [];
-
-    // for(i=0; i< 24; i++){
-    //     time.push(data.py_wierden.entry_date[i]);
-    //     temperature_2m.push(data.py_wierden.temperature[i]);
-    // }
-
-    // alert(time);
-    // alert(temperature_2m);
-
-    // removeElement("div1");
-    
-    // addElement("div1", "canvas", location);
-        
-    // new Chart(document.getElementById(location),{
-    //     type: 'line',
-    //     data:{
-    //         labels: time,
-    //         datasets: [
-    //             {
-    //                 label: "Temperature",
-    //                 data: temperature_2m
-    //             }
-    //         ]
-    //     },
-    //     options: {
-    //         legend: { display: false },
-    //         plugins:{
-    //             title: {
-    //                 display: true,
-    //                 text: "TestLocation"
-    //             }
-    //         }
-    //     }
-    // });
-    
-    //response = await fetch(someURL);
-    //console.log(response);
-    //const data = await response.json();
-    //console.log(response);
-    //length = data.hourly.time.length;
-    //console.log(response);
-
     
 }
